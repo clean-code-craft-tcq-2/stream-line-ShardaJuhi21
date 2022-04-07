ce88547d049c8baccd04b3f9a4ba87aa6748bd3e
ShardaJuhi21 <91040711+ShardaJuhi21@users.noreply.github.com>
2022-04-07 08:37:30 +0530

    Update CurrentManagement.c

--- CurrentManagement.c
+++ CurrentManagement.c
@@ -130,7 +130,7 @@ int roundOffCurrentValue(float currentValue){
 }
 
 int AbsoluteValue(int roundedChargingCurrentValue){
-	CurrentValue = abs(roundedChargingCurrentValue);
+	int CurrentValue = abs(roundedChargingCurrentValue);
 	return chargingCurrentValue;
 }
 
